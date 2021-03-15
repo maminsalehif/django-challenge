3a5a0dfe99058a72f81c5b51f7c0bc60634d455c
Amin Salehi <m.amin.salehi.f@gmail.com>
2021-03-15 11:07:00 +0330

    command and query abstract classes added.

---
+++ shared/cqrs.py
@@ -0,0 +1,38 @@
+from abc import ABCMeta
+from typing import Dict
+
+from shared.valueobject import DomainError
+from shared.result import Result
+
+"""
+    Command and Query class assures the handler that the request DTO is valid.    
+"""
+
+
+class CommandABC(metaclass=ABCMeta):
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classmethod
+    def create(cls, a_dict: Dict) -> Result:
+        try:
+            command = cls(**a_dict)
+            return Result.ok(command)
+        except ValueError as e:
+            return Result.fail(DomainError("ValidationError", e))
+
+
+class QueryABC(metaclass=ABCMeta):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classmethod
+    def create(cls, a_dict: Dict) -> Result:
+        try:
+            command = cls(**a_dict)
+            return Result.ok(command)
+        except ValueError as e:
+            return Result.fail(DomainError("ValidationError", e))
