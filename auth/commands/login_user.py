0511a4fecc094b1f3adbb7f5f7d3394e75f0dd58
Amin Salehi <m.amin.salehi.f@gmail.com>
2021-03-15 12:07:03 +0330

    login user command and command handler is added.

---
+++ auth/commands/login_user.py
@@ -0,0 +1,34 @@
+from database.abc import UnitOfWorkABC
+from shared.cqrs import CommandABC
+from shared.valueobject import DomainError
+from shared.result import Result
+from shared.validators import String
+
+
+class LoginUserCommand(CommandABC):
+    phone_number = String(minsize=11, maxsize=11)
+    password = String(minsize=8, maxsize=32)
+
+    def __init__(self, phone_number: str, password: str):
+        super().__init__(phone_number=phone_number, password=password)
+
+
+class LoginUserCommandHandler:
+    def __init__(self, unit_of_work: UnitOfWorkABC):
+        self._uow = unit_of_work
+
+    def execute(self, command: LoginUserCommand):
+        user_or_error = self._uow.user_credential_repository.get_user_credential_by_phone_number(
+            phone_number=command.phone_number
+        )
+        if user_or_error.is_failure:
+            return Result.fail(DomainError("WrongPhoneNumberOrPassword", None))
+
+        # TODO check hashed password
+        if command.password != user_or_error.value.hashed_password:
+            return Result.fail(DomainError("WrongPhoneNumberOrPassword", None))
+
+        # Warning: login usecase is not an command in CQRS pattern
+        # TODO create jwt token
+        jwt = ""
+        return Result.ok(jwt)
